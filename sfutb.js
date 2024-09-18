e3aeaf266001245356e309f056dc3d7a7026dbf8
AUG__DOG <augustdagostino@icloud.com>
2024-09-18 09:39:41 -0500

    Update sfutb.js
    more stuff

--- sfutb.js
+++ sfutb.js
@@ -1,22 +1,40 @@
-import 'sfutb.css'
-import { resources } from "./resources.js";
+import 'sfutb.css';
+import { resources } from "resources.js";
 
-const canvas = document/querySelector("#game-canvas");
+const canvas = document/querySelector("game-canvas");
 const ctx = canvas.getContext("2d");
 
-const draw = () => {
-  const sky = resources.images.sky;
-  if (sky.isLoaded) {
-  ctx.drawImage(sky.image, 0, 0)
-  }
+const skySprite = new Sprite({
+  resource: resources.images.sky,
+  frameSize: new Vector2(320, 180)
+});
 
-  const ground = resources.images.sky
-  if (sky.isLoaded) {
-  ctx.drawImage(ground.image, 0, 0)
-  }
-}
+const groundsprite = new Sprite({
+  resource: resources.images.ground,
+  frameSize: new Vector2(320, 180)
+});
+
+const Hero = new Sprite({
+  resource: resources.images.hero,
+  frameSize: new Vector2(32, 32),
+  hFrames: 3,
+  vFrames: 8,
+  frame: 1,
+});
+
+const heroPos = new Vector2(16*5, 16*5);
+
+const draw = () => {
+  skySprite.drawImage(ctx, 0, 0);
+  groundSprite.drawImage(ctx, 0, 0);
+  
+  hero.drawImage(ctx, heroPos.x, heroPos.y);
+};
+
 
 setInterval(() => {
-  console.log("draw")
-  draw()
-}, 300)
+  console.log("draw");
+  draw();
+}, 300);
+
+draw();
