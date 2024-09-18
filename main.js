15f533d94525ff1f88578d9a4a8a226dc2921927
AUG__DOG <augustdagostino@icloud.com>
2024-09-18 11:01:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 import 'main.css';
-import { resources } from "resources.js";
+import { Resources } from "source/resources.js";
 
 const canvas = document/querySelector("game-canvas");
 const ctx = canvas.getContext("2d");
 
 const skySprite = new Sprite({
-  resource: resources.images.sky,
+  resource: Resources.images.sky,
   frameSize: new Vector2(320, 180)
 });
 
-const groundsprite = new Sprite({
-  resource: resources.images.ground,
+const groundSprite = new Sprite({
+  resource: Resources.images.ground,
   frameSize: new Vector2(320, 180)
 });
 
 const Hero = new Sprite({
-  resource: resources.images.hero,
+  resource: Resources.images.hero,
   frameSize: new Vector2(32, 32),
   hFrames: 3,
   vFrames: 8,
   frame: 1,
 });
+
+const Shadow = new Sprite({
+  resource: Resources.images.shadow,
+  frameSize: new Vector2(32, 32)
+})
 
 const heroPos = new Vector2(16*5, 16*5);
 
@@ -28,13 +33,16 @@ const draw = () => {
   skySprite.drawImage(ctx, 0, 0);
   groundSprite.drawImage(ctx, 0, 0);
   
-  hero.drawImage(ctx, heroPos.x, heroPos.y);
+  const herOffset = new Vector2(-8, -21);
+  const heroPosX = heroPos.x + heroOffset.x;
+  const heroPosY = heroPos.y + heroOffset.y;
+  
+  shadow.dramImage(ctx, heroPosX, heroPosY);
+  hero.drawImage(ctx, heroPosX, heroPosY);
 };
 
 
 setInterval(() => {
-  console.log("draw");
+  hero.frame += 1;
   draw();
 }, 300);
-
-draw();
