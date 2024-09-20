7a7afa116d491374b99a88f2ee0cb666d851ae7a
BuildTools <unconfigured@null.spigotmc.org>
2024-09-20 17:24:33 -0500

    idk anymore

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-import 'main.css';
-import { Resources } from "resources.js";
+import { resources } from "./source/resources.js";
 
 const canvas = document.querySelector("game-canvas");
-const ctx = canvas.getContext("2d");
+
+const draw = () => {
+  const sky = resources.images.sky;
+  if (sky.isLoaded) {
+    ctx.drawImage(sky.image, 0, 0)
+  }
+}
 
 const skySprite = new Sprite({
   resource: Resources.images.sky,
